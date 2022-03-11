a889db8ff339dd7e02664c7a22016c85ff61a039
rfannii <100438489+rfannii@users.noreply.github.com>
2022-03-11 23:22:01 +0100

    Update 14drill.cpp

--- 14drill.cpp
+++ 14drill.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <string>
+#include "std_lib_facilities.h"
 
-using namespace std;
 
 class B1 {
 		public:
 				virtual void vf() {cout <<"B1::vf()"<<endl;}
 				void f() {cout << "B1::f()" <<endl;}
-				class D2 : public D1  { //virtualis függvény (absztrakt osztály lett)
+				class D2 : public D1  { 
 };
 
-class D1 : public B1 { //ő is absztrakt
+class D1 : public B1 { 
 		public:
 		void vf() {cout << "D1::vf()"<<endl;}
 		void f() {cout << "D1::f()" <<endl;}
@@ -18,12 +16,12 @@ class D1 : public B1 { //ő is absztrakt
 };
 class D2 : public D1  {
 		public:
-				void pvf(){cout << "D2::pvf()" <<endl;} //felülírjuk a függvényt
+				void pvf(){cout << "D2::pvf()" <<endl;} 
 };
 
 class B2 {
 		public:
-				virtual void pvf() = 0; // tisztán virtuális
+				virtual void pvf() = 0; 
 };
 				
 class D21 : public B2  {
@@ -45,20 +43,6 @@ void f(B2& b2)
 }
 
 int main(){
-		/*B1 b1;
-		b1.vf();
-		b1.f()*/ // nem tudjuk példányosítani mert absztrakt
-		
-		/*D1 d1;
-		d1.vf();
-		d1.f();
-		d1.f();
-		d1.f();
-		d1.f();
-		
-		B1& b1ref = d1;
-		b1ref.vf();
-		b1ref.f();*/
 		
 		D2 d2;
 		d2.f();
