d2a33fae740176cd0546fa209586b7047fd77ace
rfannii <100438489+rfannii@users.noreply.github.com>
2022-05-09 23:38:17 +0200

    Update 15drill.cpp

--- 15drill.cpp
+++ 15drill.cpp
@@ -5,54 +5,52 @@
 #include "vector"
 
 class Person{
-	public:
-		Person(){};
-		Person(string f, string l, int a): f{f}, l{l}, a{a} {
-			if(a<0 || a>=150){
-				throw runtime_error("Invalid age");
-			}
-			string n= f+l;
-			for(int i=0; i<n.length(); i++){
-				if(n[i]==';' || n[i]==':' || n[i]=='"' || n[i]=='$' || n[i]=='['|| n[i]==']'|| n[i]=='*'|| n[i]=='&'|| n[i]=='^' || n[i]=='%'|| n[i]=='@'|| n[i]=='!'|| n[i]=='#'|| n[i]=='\''){
-					throw runtime_error("Invalid name");
-				}
-			}
+public:
+Person(){};
+Person(string f, string l, int a): f{f}, l{l}, a{a} {
+if(a<0 || a>=150){
+throw runtime_error("Invalid age");
+}
+string n= f+l;
+for(int i=0; i<n.length(); i++){
+if(n[i]==';' || n[i]==':' || n[i]=='"' || n[i]=='$' || n[i]=='['|| n[i]==']'|| n[i]=='*'|| n[i]=='&'|| n[i]=='^' || n[i]=='%'|| n[i]=='@'|| n[i]=='!'|| n[i]=='#'|| n[i]=='\''){
+throw runtime_error("Invalid name");
+}
+}
 		
-		};
-		string first() {return f;}
-		string last() {return l;}
-		int age() { return a; }
-	private:
-		
-		string f, l;
-		int a;
+};
+string first() {return f;}
+string last() {return l;}
+int age() { return a; }
+private:
+string f, l;
+int a;
 };
 
 ostream & operator<<(ostream & os, Person & s){  return os << s.first() <<" "<< s.last() << " "<<s.age()<<endl; }
 istream & operator>>(istream & is, Person & s){ 
-	string f,l;
-	int a;
-	is >> f >> l >>a;
-	s=Person(f,l,a);
-	return is;
-	}
+string f,l;
+int a;
+is >> f >> l >>a;
+s=Person(f,l,a);
+return is;
+}
 
 int main(){
+Person p=Person("Goofy","hmm",63);
+cout<<p.first() <<" "<<p.last() <<" "<< p.age() << endl;
 
- Person p=Person("Goofy","hmm",63);
- cout<<p.first() <<" "<<p.last() <<" "<< p.age() << endl;
+Person p2;
+cin>>p2;
+cout<<p2;
 
- Person p2;
- cin>>p2;
- cout<<p2;
-
- vector<Person> vec;
- for(Person p; cin>>p;){
+vector<Person> vec;
+for(Person p; cin>>p;){
  	if(p.first()=="end") break;
  	vec.push_back(p);
  }
- for(Person p : vec){
- 	cout<<p<<endl;
+for(Person p : vec){
+cout<<p<<endl;
  }
 
 }
