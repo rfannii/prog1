ada678680a19e312f13d55baabeb76bd90ddc2f9
rfannii <100438489+rfannii@users.noreply.github.com>
2022-05-09 23:37:04 +0200

    Update 14drill.cpp

--- 14drill.cpp
+++ 14drill.cpp
@@ -2,63 +2,63 @@
 
 
 class B1 {
-		public:
-				virtual void vf() {cout <<"B1::vf()"<<endl;}
-				void f() {cout << "B1::f()" <<endl;}
-				class D2 : public D1  { 
+public:
+virtual void vf() {cout <<"B1::vf()"<<endl;}
+void f() {cout << "B1::f()" <<endl;}
+class D2 : public D1  { 
 };
 
 class D1 : public B1 { 
-		public:
-		void vf() {cout << "D1::vf()"<<endl;}
-		void f() {cout << "D1::f()" <<endl;}
+public:
+void vf() {cout << "D1::vf()"<<endl;}
+void f() {cout << "D1::f()" <<endl;}
 
 };
 class D2 : public D1  {
-		public:
-				void pvf(){cout << "D2::pvf()" <<endl;} 
+public:
+void pvf(){cout << "D2::pvf()" <<endl;} 
 };
 
 class B2 {
-		public:
-				virtual void pvf() = 0; 
+public:
+virtual void pvf() = 0; 
 };
 				
 class D21 : public B2  {
-		public:
-				string s;
-				void pvf() { cout<< s << endl;}
+public:
+string s;
+void pvf() { cout<< s << endl;}
 };
 
 class D22: public B2 {
-		public:
-				int n;
-				void pvf(){ cout << n << endl;}
+public:
+int n;
+void pvf(){ cout << n << endl;}
 				
 				
 };
 void f(B2& b2)
 {
-		b2.pvf();
+b2.pvf();
 }
 
 int main(){
 		
-		D2 d2;
-		d2.f();
-		d2.vf();
-		d2.pvf();
+D2 d2;
+d2.f();
+d2.vf();
+d2.pvf();
 		
-		D21 d21;
-		d21.s = "d21.s";
-		d21.pvf();
+D21 d21;
+d21.s = "d21.s";
+d21.pvf();
 		
-		D22 d22;
-		d22.n = 22;
-		d22.pvf();
+D22 d22;
+d22.n = 22;
+d22.pvf();
 		
-		f(d21);
-		f(d22);
+f(d21);
+f(d22);
 		
 }
 		
